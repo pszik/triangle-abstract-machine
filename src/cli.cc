1b79e553a095a5b49072ca3225eaadfcc989dd55
Ian Knight <ian.knight2@nottingham.ac.uk>
2025-08-06 12:18:10 +0100

    Update documentation and stick closer to LLVM style

--- src/cli.cc
+++ src/cli.cc
@@ -1,85 +1,123 @@
-/*
- * This file is part of tam-cpp, copyright (c) Ian Knight 2025.
- *
- * tam-cpp is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * tam-cpp is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with tam-cpp. If not, see <https://www.gnu.org/licenses/>.
- */
-
+//===-----------------------------------------------------------------------===//
+//
+// This file is part of tam-cpp, copyright (c) Ian Knight 2025.
+//
+// tam-cpp is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// tam-cpp is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with tam-cpp. If not, see <https://www.gnu.org/licenses/>.
+//
+//===-----------------------------------------------------------------------===//
+//
 /// @file cli.cc
-/// Implements the CLI-parsing interface.
+/// This file defines the `parseCli` function used for processing command-line
+/// arguments. It also defines some static auxiliary functions used in the
+/// process.
+//
+//===-----------------------------------------------------------------------===//
+
+#include <tam/cli.h>
 
 #include <cstring>
 #include <optional>
-#include <tam/cli.h>
 
-namespace tam {
+using namespace tam;
 
+/// Parse arguments when a single argument was given.
+///
+/// Valid arguments in this case are the help flag, or a filename by itself.
+///
+/// @param Argv the arguments to parse with the program name removed
+/// @returns the `CliArgs` if parsing succeeded or empty if not
 static std::optional<CliArgs> parse1(const char **Argv);
+
+/// Parse arguments when two argumens were given.
+///
+/// Valid arguments in this case are either the trace or step flag, followed by
+/// a filename.
+///
+/// @param Argv the arguments to parse with the program name removed
+/// @returns the `CliArgs` if parsing succeeded or empty if not
 static std::optional<CliArgs> parse2(const char **Argv);
+
+/// Parse arguments when three arguments were given.
+///
+/// The only valid arguments here consist of both trace and step flags
+/// separately, followed by a filename.
+///
+/// @param Argv the arguments to parse with the program name removed
+/// @returns the `CliArgs` if parsing succeeded or empty if not
 static std::optional<CliArgs> parse3(const char **Argv);
 
-std::optional<CliArgs> parseCli(int Argc, const char **Argv) {
+/// Determines the number of arguments given and calls the appropriate
+/// auxiliary function based on that.
+std::optional<CliArgs> tam::parseCli(int Argc, const char **Argv) {
     switch (Argc) {
     case 2:
         return parse1(Argv + 1);
-        break;
     case 3:
         return parse2(Argv + 1);
-        break;
     case 4:
         return parse3(Argv + 1);
-        break;
     default:
         return {};
     }
 }
 
+/// Indicates if an argument string matches the trace option.
+///
+/// @param Arg the argument
+/// @returns `true` if the argument is a trace flag
 static inline bool isTraceArg(const char *Arg) {
     return (strcmp(Arg, "-t") == 0 || strcmp(Arg, "--trace") == 0);
 }
 
+/// Indicates if an argument string matches the step option.
+///
+/// @param Arg the argument
+/// @returns `true` if the argument is a step flag
 static inline bool isStepArg(const char *Arg) {
     return (strcmp(Arg, "-s") == 0 || strcmp(Arg, "--step") == 0);
 }
 
 static std::optional<CliArgs> parse1(const char **Argv) {
-    if (isTraceArg(Argv[0]) || isStepArg(Argv[0])) {
+    if (isTraceArg(Argv[0]) || isStepArg(Argv[0]))
         return {};
-    }
-    if (strcmp(Argv[0], "-h") == 0 || strcmp(Argv[0], "--help") == 0) {
-        return CliArgs{"", false, false, true};
-    }
-    return CliArgs{std::string(Argv[0]), false, false, false};
+
+    if (strcmp(Argv[0], "-h") == 0 || strcmp(Argv[0], "--help") == 0)
+        return CliArgs{.Filename = "", .Help = true};
+
+    return CliArgs{std::string(Argv[0])};
 }
 
 static std::optional<CliArgs> parse2(const char **Argv) {
-    if (isTraceArg(Argv[0])) {
-        return CliArgs{std::string(Argv[1]), true, false, false};
-    } else if (isStepArg(Argv[0])) {
-        return CliArgs{std::string(Argv[1]), false, true, false};
-    } else if (strcmp(Argv[0], "-ts") == 0 || strcmp(Argv[0], "-st") == 0) {
-        return CliArgs{std::string(Argv[2]), true, true, false};
-    } else {
-        return {};
+    if (isTraceArg(Argv[0]))
+        return CliArgs{.Filename = std::string(Argv[1]), .Trace = true};
+
+    if (isStepArg(Argv[0]))
+        return CliArgs{.Filename = std::string(Argv[1]), .Step = true};
+
+    if (strcmp(Argv[0], "-ts") == 0 || strcmp(Argv[0], "-st") == 0) {
+        return CliArgs{
+            .Filename = std::string(Argv[2]), .Trace = true, .Step = true};
     }
+
+    return {};
 }
 
 static std::optional<CliArgs> parse3(const char **Argv) {
     if ((isTraceArg(Argv[0]) && isStepArg(Argv[1])) ||
         (isStepArg(Argv[0]) && isTraceArg(Argv[1]))) {
-        return CliArgs{std::string(Argv[2]), true, true, false};
-    } else {
-        return {};
+        return CliArgs{
+            .Filename = std::string(Argv[2]), .Trace = true, .Step = true};
     }
-}
 
-} // namespace tam
+    return {};
+}
--- src/heap.cc
+++ src/heap.cc
@@ -1,30 +1,38 @@
-/*
- * This file is part of tam-cpp, copyright (c) Ian Knight 2025.
- *
- * tam-cpp is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * tam-cpp is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with tam-cpp. If not, see <https://www.gnu.org/licenses/>.
- */
-
+//===-----------------------------------------------------------------------===//
+//
+// This file is part of tam-cpp, copyright (c) Ian Knight 2025.
+//
+// tam-cpp is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// tam-cpp is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with tam-cpp. If not, see <https://www.gnu.org/licenses/>.
+//
+//===-----------------------------------------------------------------------===//
+//
 /// @file heap.cc
-/// Implements logic for managing the heap on a TAM emulator.
+/// This file defines the `allocate` and `free` methods of `TamEmulator`.
+//
+//===-----------------------------------------------------------------------===//
+
+#include <tam/tam.h>
+
+#include <tam/error.h>
 
 #include <cassert>
 #include <map>
-#include <tam/error.h>
-#include <tam/tam.h>
 
-namespace tam {
+using namespace tam;
 
-TamAddr TamEmulator::allocate(int N) {
+/// Iterates through all existing free blocks in the heap to try and find one of
+/// the correct size. If none is found, the heap is expanded.
+tam::TamAddr tam::TamEmulator::allocate(int N) {
     // try to find unallocated space inside heap
     for (auto BlockIter = this->FreeBlocks.begin();
          BlockIter != this->FreeBlocks.end(); ++BlockIter) {
@@ -54,26 +62,26 @@ TamAddr TamEmulator::allocate(int N) {
     return this->Registers[HT] + 1;
 }
 
-void TamEmulator::free(TamAddr Addr, TamData Size) {
-    if (Addr <= this->Registers[HT]) {
+/// Attempts to locate an allocated block of the given address and size.
+/// If the block was at the end of the heap then the heap is contracted,
+/// otherwise the freed block is added to the list of available blocks.
+void tam::TamEmulator::free(TamAddr Addr, TamData Size) {
+    if (Addr <= this->Registers[HT])
         throw runtimeError(EK_DataAccessViolation, this->Registers[CP] - 1);
-    }
 
-    if (!this->AllocatedBlocks.count(Addr)) {
+    if (!this->AllocatedBlocks.count(Addr))
         throw runtimeError(EK_DataAccessViolation, this->Registers[CP] - 1);
-    }
 
-    for (auto BlockIter = this->AllocatedBlocks.begin();
-         BlockIter != this->AllocatedBlocks.end(); ++BlockIter) {
+    for (auto BlockIter = this->AllocatedBlocks.begin(),
+              E = this->AllocatedBlocks.end();
+         BlockIter != E; ++BlockIter) {
         assert(BlockIter->first > this->Registers[HT]);
 
-        if (BlockIter->first != Addr) {
+        if (BlockIter->first != Addr)
             continue;
-        }
 
-        if (BlockIter->second != Size) {
+        if (BlockIter->second != Size)
             throw runtimeError(EK_DataAccessViolation, this->Registers[CP] - 1);
-        }
 
         this->AllocatedBlocks.erase(BlockIter);
         if (Addr == this->Registers[HT] + 1) {
@@ -86,5 +94,3 @@ void TamEmulator::free(TamAddr Addr, TamData Size) {
         break;
     }
 }
-
-} // namespace tam
