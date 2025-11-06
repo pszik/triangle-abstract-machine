7789b70d96b8107a3c5b099bc1ae27816ff2ed85
Ian Knight <ian.knight2@nottingham.ac.uk>
2025-11-06 09:56:44 +0000

    Refactor TestSimpleProgram to check correct output

--- include/tam/test/integration_test.h
+++ include/tam/test/integration_test.h
@@ -41,6 +41,16 @@ class EmulatorTest : public testing::Test, public tam::TamEmulator {
         this->registers[tam::ST] = data.size();
         assert(this->registers[tam::ST] == data.size());
     }
+
+    void setInstream(FILE* instream) {
+        assert(instream);
+        this->instream = instream;
+    }
+
+    void setOutstream(FILE* outstream) {
+        assert(outstream);
+        this->outstream = outstream;
+    }
 };
 
 #endif  // INTEGRATION_TEST_H__
