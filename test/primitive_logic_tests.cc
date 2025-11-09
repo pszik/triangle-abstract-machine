d059dd753aefbecc5ad8d3b29e9bcb138e4700f2
Ian Knight <ian.knight2@nottingham.ac.uk>
2025-11-09 18:00:07 +0000

    Ensure workflow inits rapidcheck submodule

--- test/primitive_logic_tests.cc
+++ test/primitive_logic_tests.cc
@@ -1,30 +1,29 @@
-#include <vector>
-
 #include "tam/tam.h"
 #include "tam/test/integration_test.h"
 
 #include "gtest/gtest.h"
 #include "rapidcheck/gtest.h"
 
-
 class PrimitiveLogicTests : public EmulatorTest {};
 
-RC_GTEST_FIXTURE_PROP(PrimitiveLogicTests, TestAnd, (tam::TamData l, tam::TamData r)) {
-  this->PushData(l);
-  this->PushData(r);
+RC_GTEST_FIXTURE_PROP(PrimitiveLogicTests, TestAnd,
+                      (tam::TamData l, tam::TamData r)) {
+    this->PushData(l);
+    this->PushData(r);
 
-  ASSERT_NO_THROW(this->PrimitiveAnd());
+    ASSERT_NO_THROW(this->PrimitiveAnd());
 
-  ASSERT_EQ(l && r, this->data_store_[0]);
+    ASSERT_EQ(l && r, this->data_store_[0]);
 }
 
-RC_GTEST_FIXTURE_PROP(PrimitiveLogicTests, TestOr, (tam::TamData l, tam::TamData r)) {
-  this->PushData(l);
-  this->PushData(r);
+RC_GTEST_FIXTURE_PROP(PrimitiveLogicTests, TestOr,
+                      (tam::TamData l, tam::TamData r)) {
+    this->PushData(l);
+    this->PushData(r);
 
-  ASSERT_NO_THROW(this->PrimitiveOr());
+    ASSERT_NO_THROW(this->PrimitiveOr());
 
-  ASSERT_EQ(l || r, this->data_store_[0]);
+    ASSERT_EQ(l || r, this->data_store_[0]);
 }
 
 TEST_F(PrimitiveLogicTests, TestNotTrue) {
